7ccc409e6e1edf1e7a93a7bc83ed374ff97a3b2c
Devanshu07 <devasrivastava21@gmail.com>
2023-04-29 20:43:17 +0530

    9th commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,18 @@ import Card from '../UI/card.js'
 import React from 'react';
 
 const ExpenseItem = (props) => {
-    
+    // function clickHandler() {}
+    const clickHandler = () => {
+        console.log('Clicked!!!');
+    }
     return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date} />
         <div className='expense-item__descrption'>
             <h2>{props.title}</h2>
-        <div className='expense-item__price'>₹{props.amount}</div>
+            <div className='expense-item__price'>₹{props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
     );
 }
