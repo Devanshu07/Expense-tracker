02bb1756c39fb177f04e690ce84a10c70a472010
Devanshu07 <devasrivastava21@gmail.com>
2023-05-22 00:25:16 +0530

    form update

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -41,7 +41,13 @@ const dateChangeHandler = (event) => {
 };
   const submitHandler = (event) => {
    event.preventDefault();
-  }
+   const expenseData = {
+    title: enteredTitle,
+    amount: enteredAmount,
+    date: new Date(enteredDate)
+   };
+   console.log(expenseData);
+  };
   return (
   <form onSubmit={submitHandler}>
     <div className="new-expense__control">
