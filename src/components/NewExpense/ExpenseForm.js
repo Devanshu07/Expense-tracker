f61a349127cef23ed48e498fc81761069cbd3d9b
Devanshu07 <devasrivastava21@gmail.com>
2023-05-24 23:56:20 +0530

    Form updation

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -45,7 +45,7 @@ const ExpenseForm = (props) => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
-    props.onSaveExpenseData();
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
